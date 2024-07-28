94caccc2fdb85e205b8d9e63338cbcedc701504b
Francisco Rocha <122055396+FrancisoRocha@users.noreply.github.com>
2024-07-28 14:11:24 -0600

    footer con img

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import Boton from "./Components/Boton/Boton";
 import Card from "./Components/Card/Card";
 import Header from "./Components/Header/Header";
-import Navbar from "./Components/Navbar/Navbar"
+import Imagenes from "./Components/Imagen/Imagen";
+import Navbar from "./Components/Navbar/Navbar";
 import TItulo from "./Components/TItulo/Titulo";
 import Texto from "./Components/Texto/Texto";
 
 function App() {
-
   return (
     <>
       <Navbar />
       <Header />
-      <TItulo/>
+      <TItulo />
       <Texto />
       <Boton />
       <Card />
+      <Imagenes />
     </>
-  )
+  );
 }
 
 export default App;
---
+++ src/Components/Imagen/Imagen.jsx
@@ -0,0 +1,16 @@
+import "./Imagen.css";
+
+function Imagenes() {
+  return (
+    <>
+      <img src="../public/img/Bitmap1.png" alt="Imagen 1" className="img-one" />
+      <p className="number-one">01</p>
+      <h4 className="title-img">Reviving Retro PCs</h4>
+      <p className="text-uno">
+        What happens when old PCs are given modern upgrades?
+      </p>
+    </>
+  );
+}
+
+export default Imagenes;
